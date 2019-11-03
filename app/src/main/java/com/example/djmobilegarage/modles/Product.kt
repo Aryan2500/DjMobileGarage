5e15a3ab16c53a0cb7f8b3f7333cfe0019e1c3aa
Aryan <arunmaurya2500@gmail.com>
2019-11-03 19:31:42 +0530

    recycler layout made

--- app/src/main/java/com/example/djmobilegarage/modles/Product.kt
+++ app/src/main/java/com/example/djmobilegarage/modles/Product.kt
@@ -4,7 +4,7 @@ data class Product(
 
     val picture: String,
     val brand: String,
-    val name_model: String,
+    val model_name: String,
     val inStock: Boolean,
     val mrp: String,
     val price_after_discount:String ,
